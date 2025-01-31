c9bb38f3c7666ba74dd7d7b68a5d76ad37358d27
Krishan Kumar <70051891+imkrrish@users.noreply.github.com>
2025-02-01 00:16:39 +0530

    config shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+    },
+  },
 });
